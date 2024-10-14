09e420f5171c89ec0e0ce339c58bbfcbab99efdd
Yunjia Wang <yunjiw1@gmail.com>
2024-10-13 20:52:20 -0400

    08 finished

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,31 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
-  
-  
+  const [num, setNum] = useState(0);
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    const data = new FormData(e.target);
+    setNum(parseInt(data.get("num")));
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <main>
+      <section>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' method='GET' onSubmit={onSubmit}>
+        <label htmlFor='num'>Paragraphs</label>
+        <input type='number' id="num" name="num" defaultValue='1' max={data.length} min='1' />
+        <button className='btn'>Generate</button>
+      </form>
+      {data
+        .slice(0, num)
+        .map((p, i) => <p key={i} className='result'>{p}</p>)}
+      </section>
+    </main>
+  )
 }
 
 export default App;
