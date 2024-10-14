d8c516dd837dbda03483035b190bbc9fe52397b0
Yunjia Wang <yunjiw1@gmail.com>
2024-10-13 21:21:28 -0400

    11 finished

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,7 +4,19 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return (
+    <nav className="nav-header">
+      <div className="logo">
+        <img src={logo}/>
+      </div>
+      <div className="links">
+        {links.map(l => <a key={l.id} className="nav-toggle" href={l.url}>{l.text}</a>)}
+      </div>
+      <div className="social-icons">
+        {social.map(s => <a key={s.id} href={s.url}>{s.icon}</a>)}
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
